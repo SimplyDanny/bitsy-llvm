085041f36bbd7995f04ee8df03c7232c3f188989
Danny Moesch <danny.moesch@icloud.com>
2020-11-03 15:18:48 +0100

    Move friend function implementation outside class

--- include/helper/ConsolePrinter.hpp
+++ include/helper/ConsolePrinter.hpp
@@ -11,18 +11,21 @@ class ConsolePrinter {
     bool last_was_endl = true;
 
   public:
-    template <typename T>
-    friend ConsolePrinter& operator<<(ConsolePrinter& printer, const T output) {
-        if (printer.last_was_endl) {
-            std::cout << std::string(2 * printer.indent, ' ');
-        }
-        std::cout << output;
-        printer.last_was_endl = false;
-        return printer;
-    };
+    template <class T>
+    friend ConsolePrinter& operator<<(ConsolePrinter& printer, const T output);
 
     void add_endl();
     void indented(const NestedPrinter& nested_printer);
+};
+
+template <class T>
+ConsolePrinter& operator<<(ConsolePrinter& printer, const T output) {
+    if (printer.last_was_endl) {
+        std::cout << std::string(2 * printer.indent, ' ');
+    }
+    std::cout << output;
+    printer.last_was_endl = false;
+    return printer;
 };
 
 extern ConsolePrinter cout;
