06927ad628d7485bd38abb39592a9c0be4c0b1b5
Danny Moesch <danny.moesch@icloud.com>
2020-11-11 23:32:19 +0100

    Use 'override' keyword

--- include/codegen/CodeGenerator.hpp
+++ include/codegen/CodeGenerator.hpp
@@ -23,22 +23,21 @@ class CodeGenerator : public ASTVisitor<llvm::Value*> {
         : builder(context), module(std::make_unique<llvm::Module>("Bitsy Program", context)),
           module_processor(module_processor) {}
 
-  public:
     using ASTVisitor<llvm::Value*>::visit;
 
   private:
-    void visit(const Program* program);
-    void visit(const Block* block);
-    void visit(const IfStatement* if_statement);
-    void visit(const LoopStatement* loop_statement);
-    void visit(const PrintStatement* print_statement);
-    void visit(const ReadStatement* read_statement);
-    void visit(const AssignmentStatement* assignment_statement);
-    void visit(const BreakStatement* break_statement);
+    void visit(const Program* program) override;
+    void visit(const Block* block) override;
+    void visit(const IfStatement* if_statement) override;
+    void visit(const LoopStatement* loop_statement) override;
+    void visit(const PrintStatement* print_statement) override;
+    void visit(const ReadStatement* read_statement) override;
+    void visit(const AssignmentStatement* assignment_statement) override;
+    void visit(const BreakStatement* break_statement) override;
 
-    llvm::Value* visit(const NumberExpression* number_expression);
-    llvm::Value* visit(const VariableExpression* variable_expression);
-    llvm::Value* visit(const BinaryOperationExpression* binary_operation_expression);
+    llvm::Value* visit(const NumberExpression* number_expression) override;
+    llvm::Value* visit(const VariableExpression* variable_expression) override;
+    llvm::Value* visit(const BinaryOperationExpression* binary_operation_expression) override;
 
     llvm::Value* allocate_variable(const std::string& name);
     llvm::Value* create_if_condition(const IfStatement* expression);
