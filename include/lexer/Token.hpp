d058fa52414ded1cd68e228460b5cf80b7b0bb8b
Danny Moesch <danny.moesch@icloud.com>
2020-11-15 21:17:16 +0100

    Do not repeat tokens

--- include/lexer/Token.hpp
+++ include/lexer/Token.hpp
@@ -12,6 +12,8 @@ struct Token {
     Token(TokenType type, std::string value)
         : type(type)
         , value(std::move(value)) {}
+    Token(TokenType type, char value)
+        : Token(type, std::string(1, value)) {}
 
     void print() const;
 };
